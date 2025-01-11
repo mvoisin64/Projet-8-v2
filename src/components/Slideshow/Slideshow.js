1eeccb8c044ae6345ecfb358c1f084e5b51799c4
mvoisin64 <mvoisin92@gmail.com>
2025-01-11 17:50:08 +0100

    composant Slideshow crée

---
+++ src/components/Slideshow/Slideshow.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import logements from '../data/logement.json'; // Importer le fichier JSON
+import './Slideshow.scss'
+
+
+const Slideshow = ({ image }) => {
+    return (
+        <div className='slideshow'>
+            <div className='slideshow-conteneur'>
+            </div>
+        </div>
+
+
+
+    )
+
+}
