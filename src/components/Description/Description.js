6e3082547bf94a8ff7ede7872e239f9af7a25fd6
mvoisin64 <mvoisin92@gmail.com>
2025-01-16 15:44:51 +0100

    composant description pour logement

---
+++ src/components/Description/Description.js
@@ -0,0 +1,19 @@
+import './Description.scss';
+
+
+const Description = () => {
+
+    return (
+        <div className="description">
+<Title/>
+<Host/>
+<Tags/>
+<Tags/>
+<Tags/>
+<Collapse/>
+<Collapse/>
+        </div>
+    );
+};
+
+export default Description;
--- src/scenes/Logement/Logement.js
+++ src/scenes/Logement/Logement.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Logement.scss';
-import Slideshow from '../../components/Slideshow/Slideshow.js'
+// import Slideshow from '../../components/Slideshow/Slideshow.js'
 import logement from '../../data/logement.json';
 
 function Logement() {
 
-  const logementId = '2345'
-
   return (
-    <div className='scene'>   
-    <Slideshow id={logementId}/>
-
+    <div className='scene'>
+      <Slideshow />
+      <Description />
     </div>
   );
 }
